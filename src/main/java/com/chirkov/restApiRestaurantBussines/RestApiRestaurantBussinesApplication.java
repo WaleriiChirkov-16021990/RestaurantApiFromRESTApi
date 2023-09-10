fea57f368db7efb45310023eb84dc1c418ef4a17
Валерий Чирков <zx84058405@live.com>
2023-09-10 13:29:35 +0300

    init and configured datasource from Spring

---
+++ src/main/java/com/chirkov/restApiRestaurantBussines/RestApiRestaurantBussinesApplication.java
@@ -0,0 +1,13 @@
+package com.chirkov.restApiRestaurantBussines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiRestaurantBussinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiRestaurantBussinesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chirkov/restApiRestaurantBussines/RestApiRestaurantBussinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chirkov.restApiRestaurantBussines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiRestaurantBussinesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
