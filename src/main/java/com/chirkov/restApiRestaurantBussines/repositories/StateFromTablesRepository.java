3b7143b427ed78582655d28d4760d9e6700eccf2
WaleriiChirkov-16021990 <zx84058405@live.com>
2023-12-29 00:15:03 +0300

    complete service from StateFromTable

--- src/main/java/com/chirkov/restApiRestaurantBussines/repositories/StateFromTablesRepository.java
+++ src/main/java/com/chirkov/restApiRestaurantBussines/repositories/StateFromTablesRepository.java
@@ -1,11 +1,12 @@
 package com.chirkov.restApiRestaurantBussines.repositories;
 
 import com.chirkov.restApiRestaurantBussines.models.StateFromTable;
+import com.chirkov.restApiRestaurantBussines.models.StateFromTableEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface StateFromTablesRepository extends JpaRepository<StateFromTable,Integer> {
     Optional<StateFromTable> findStateFromTablesByName(String tableName);
-    Optional<StateFromTable> findStateFromTablesByValue(String value);
+    Optional<StateFromTable> findStateFromTablesByValue(StateFromTableEnum value);
 }
---
+++ src/main/java/com/chirkov/restApiRestaurantBussines/services/StateFromTablesService.java
@@ -0,0 +1,54 @@
+package com.chirkov.restApiRestaurantBussines.services;
+
+import com.chirkov.restApiRestaurantBussines.models.RoleEnum;
+import com.chirkov.restApiRestaurantBussines.models.StateFromTable;
+import com.chirkov.restApiRestaurantBussines.models.StateFromTableEnum;
+import com.chirkov.restApiRestaurantBussines.repositories.StateFromTablesRepository;
+import com.chirkov.restApiRestaurantBussines.units.exceptions.StateFromTableNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+public class StateFromTablesService {
+    private final StateFromTablesRepository repository;
+
+    @Autowired
+    public StateFromTablesService(StateFromTablesRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<StateFromTable> findAll() {
+        return this.repository.findAll();
+    }
+
+    public StateFromTable getStateById(int id) {
+        Optional<StateFromTable> state = this.repository.findById(id);
+        return state.orElseThrow(StateFromTableNotFoundException::new);
+    }
+
+    public Optional<StateFromTable> getStateByName(String name) {
+        return this.repository.findStateFromTablesByName(name);
+    }
+
+    public Optional<StateFromTable> getStateByValue(String stateValue) {
+        Optional<StateFromTable> findState;
+        for (StateFromTableEnum state1 :
+                StateFromTableEnum.values()) {
+            if (state1.toString().equals(stateValue)) {
+                findState = this.repository.findStateFromTablesByValue(state1);
+                return findState;
+            }
+        }
+        throw new StateFromTableNotFoundException("State " + stateValue + ", not found");
+    }
+
+    @Transactional
+    public void save(StateFromTable state) {
+        this.repository.save(state);
+    }
+}
